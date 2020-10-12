3a0b78a31453809deda6b9099dd4637b5b960e04
CamronWalker <camronwalker@gmail.com>
2020-10-12 14:12:46 -0600

    a to an e

--- AppSpecific/FileManagement/mapshare.bat
+++ AppSpecific/FileManagement/mapshare.bat
@@ -1,4 +1,4 @@
 echo off
 REM mqDZc584*J*u&3h2
 
-net use w: "\\walker-drive\Shared Drive" "mqDZc584*J*u&3h2" /user:share /persistant:yes
+net use w: "\\walker-drive\Shared Drive" "mqDZc584*J*u&3h2" /user:share /persistent:yes
